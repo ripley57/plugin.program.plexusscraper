52f4654f4660f199d035d7d56b57e90e532a5048
Jeremy Clough <clough.jeremy@gmail.com>
2019-08-21 11:16:47 +0100

    PlexusHistoryFile tests working

---
+++ test/features/steps/plexus_history_file_steps.py
@@ -0,0 +1,38 @@
+from behave import given, when, then
+
+from plexusscraper.scraper import Scraper
+from plexusscraper.plexushistoryfile import PlexusHistoryFile
+
+
+@given(u'the following html')
+def step_impl(context):
+	context.html = context.text
+
+
+@given(u'the following list of raw urls')
+def step_impl(context):
+	context.raw_urls = context.text
+
+
+@when(u'I generate a plexus history file from the html')
+def step_impl(context):
+	scraper = Scraper()
+	context.result = scraper.extract_links_from_html(context.html)
+	ace_list = context.result['acestream']
+	sop_list = context.result['sopcast']
+	context.history_file = PlexusHistoryFile()
+	context.history_file.add_acestream_urls(ace_list)
+	context.history_file.add_sopcast_urls(sop_list)
+
+
+@when(u'I generate a plexus history file from the list of raw urls')
+def step_impl(context):
+	context.history_file = PlexusHistoryFile()
+	context.history_file.add_raw_urls(context.raw_urls)
+	print("JCDC: context.history_file.text=", context.history_file.text)
+
+
+@then(u'I expect the plexus history file to look like this')
+def step_impl(context):
+	assert(context.text == context.history_file.text)
+
